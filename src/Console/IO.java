c27b25394c3df3ba70fb500ef6735fcd0e6a07e9
Thong Nguyen <ducthong1995@berkeley.edu>
2019-03-13 20:40:09 -0700

    Project 1: AllUsers.java and IO.java

--- src/Console/IO.java
+++ src/Console/IO.java
@@ -5,6 +5,44 @@
  */
 package Console;
 
+import Model.AllUsers;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 
+
+/**
+ *
+ * @author Thong Nguyen
+ */
 public class IO {
+    // I make the writeToFile() and readFromFile() methods static so I can use 
+    // them whenever and wherever I need without the need to create an object
+    
+    // I use this fIn AddCategory and AddReminder because we want to save
+    // any new Reminder or Reminder category we just created.
+    public static void writeToFile(AllUsers allUsers) throws IOException {
+        FileOutputStream fOut = new FileOutputStream("savefile");
+        ObjectOutput oOut = new ObjectOutputStream(fOut);
+        //Store course
+        oOut.writeObject(allUsers);
+        oOut.flush();
+        oOut.close();
+        fOut.close();
+    }
+    
+    // Read saved data from fOut. This method is run at the beginning of the program
+    public static AllUsers readFromFile() throws IOException, ClassNotFoundException {
+        FileInputStream fIn = new FileInputStream("savefile");
+        ObjectInputStream oIn = new ObjectInputStream(fIn);
+        AllUsers allUsers = (AllUsers) oIn.readObject();
+        oIn.close();
+        fIn.close();
+        
+        return allUsers;
+    }
 }
+
