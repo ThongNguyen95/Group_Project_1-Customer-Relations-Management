529d5b9eca0f8aee0b1b1b3caa7a0979b2cd2cc3
Thong Nguyen <ducthong1995@berkeley.edu>
2019-03-13 21:43:55 -0700

    Project 1: MainApp.java and Quit.java

--- src/MainApp/MainApp.java
+++ src/MainApp/MainApp.java
@@ -5,12 +5,44 @@
  */
 package MainApp;
 
+import Console.IO;
+import Console.Menu;
+import Console.MenuStack;
+import Console.Quit;
+import Model.AllUsers;
+import java.io.IOException;
 
+/**
+ *
+ * @author Thong Nguyen
+ */
 public class MainApp {
 
     /**
      * @param args the command line arguments
      */
+    
+    //Thong
     public static void main(String[] args) {
+        //Create list of customers and business owner
+        AllUsers allUsers;
+        //Load data from file
+        try {
+            allUsers = IO.readFromFile();
+        
+        } catch (ClassNotFoundException | IOException o) {
+            allUsers = new AllUsers();
+        }
+        //Create the menu and options
+        MenuStack menus = new MenuStack();
+        Menu mainMenu = new Menu("Main Menu", menus);
+        mainMenu.addMenu(new Quit(allUsers));
+        mainMenu.addMenu(new SignUp(allUsers));
+        mainMenu.addMenu(new OwnerSignIn(allUsers, menus));
+        mainMenu.addMenu(new CustomerSignIn(allUsers));
+        while (true) {
+            menus.run();
+            
+        }
     }   
 }
