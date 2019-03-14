b0765f9f79cd315052ba069481494898cb54b9e4
zarni <zarni@192.168.0.25>
2019-03-13 20:59:41 -0700

    CustomerSignIn.java and OwnerSignIn.java (add one function in AllUsers.java)

--- src/MainApp/CustomerSignIn.java
+++ src/MainApp/CustomerSignIn.java
@@ -5,14 +5,48 @@
  */
 package MainApp;
 
+import static Console.Console.STDIN;
 import Console.MenuItem;
+import Model.AllUsers;
+import Model.Customer;
 
 
 public class CustomerSignIn implements MenuItem {
+        AllUsers allUsers;
+        
+    public CustomerSignIn(AllUsers _allUsers){
+        allUsers = _allUsers;
+    }
 
     @Override
     public void execute() {
-
+        Customer tempCustomer;
+        System.out.println("Enter your ID");
+        
+        
+        do{
+            
+            String str = STDIN.next();
+            tempCustomer = allUsers.getCustomerBasedOnID(str);
+            if(tempCustomer == null){
+                System.out.println("Invalid ID");
+                
+            }else {
+                while(str.equals(tempCustomer.getID())){
+                    System.out.println("Enter your Password");
+                    String str2 = STDIN.next();
+                    
+                    if(str2.equals(tempCustomer.getPassword())){
+                        //subMenu(tempCustomer);
+                        break;
+                    }
+                    else {
+                        System.out.println("Invalid password");
+                    }
+                }
+            }
+            
+        }while(tempCustomer == null);
     }
 
     @Override
