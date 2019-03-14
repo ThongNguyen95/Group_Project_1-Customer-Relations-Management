a19ac9d43b350bf75c8370cf5de4a328ecd1351b
Myo Zaw <Myo Zaw@DESKTOP-N2G8JF5>
2019-03-13 21:42:46 -0700

    Change password

--- src/Console/Menu.java
+++ src/Console/Menu.java
@@ -5,18 +5,34 @@
  */
 package Console;
 
+import static Console.Console.getChoice;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu implements MenuItem {
 
+	private String name;
+	private MenuStack menus;
+	private final List<MenuItem> options;
 
-    public void addMenu(MenuItem option) {
-    }
-
-    @Override
-    public void execute() {
-    }
-
-    @Override
-    public String toString() {
-        return null;
-    }
+	public Menu (String _name, MenuStack _menuStack){
+		name = _name;
+		menus = _menuStack;
+		menus.add(this);
+		options = new ArrayList<>();
+	}
+	
+	public void addMenu(MenuItem option){
+		options.add(option);
+	}
+	
+	@Override
+	public void execute(){
+		options.get(getChoice(options)).execute();
+	}
+	
+	@Override
+	public String toString(){
+		return name;
+	}
 }
